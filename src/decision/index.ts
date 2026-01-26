b169ff81fd13a2ed183c7199eabeb86cf5017eee
personal <o.rzazade@gmail.com>
2026-01-27 00:20:41 +0400

    chore(14-01): install date-fns and create decision module
    - Install date-fns 4.1.0 for time manipulation
    - Create src/decision/ directory structure
    - Add empty index.ts for module exports

---
+++ src/decision/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Decision support module for work recommendation.
+ *
+ * Provides time-of-day context detection and work type classification
+ * to help users decide what to work on.
+ */
+
+// Exports will be added as modules are created
